3dc7bd7ea7abfc839b99210b7f0e8e95241f849e
Tharsan S <tharsantharsan05819@gmail.com>
2026-02-13 15:24:18 +0530

    Error handling

--- src/fetchAPI.jsx
+++ src/fetchAPI.jsx
@@ -1,63 +1,32 @@
-// import React, { useEffect, useState } from "react";
-
 import { func } from "prop-types";
 import { useEffect, useState } from "react";
 
-// function FetchAPI() {
-
-//   const [data, setData] = useState(null);
-
-//   useEffect(() => {
-
-//     const fetchData = async () => {
-//       try {
-//         const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-//         const json = await res.json();
-//         console.log(json);
-//         setData(json);   // ✅ store full object
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     };
-
-//     fetchData();
-
-//   }, []);
-
-//   return (
-//     <>
-//       {data && (
-//         <>
-//           <p>UserId: {data.userId}</p>
-//           <p>Id: {data.id}</p>
-//           <p>Title: {data.title}</p>
-//           <p>Completed: {data.completed.toString()}</p>
-//         </>
-//       )}
-//     </>
-//   );
-// }
-
-// export default FetchAPI;
-
-
 function FetchAPI(){
     const[data,setData]=useState("");
+    const[error,setError]=useState("")
 
-    useEffect(()=>{
-        const apidata=async()=>{
-            const info= await fetch('https://jsonplaceholder.typicode.com/todos/1')
-            const json= await info.json();
-            console.log(json);
-            setData(json);
-        }
-        apidata();
-        
-    },[])
+useEffect(() => {
+
+  const apidata = async () => {
+    try {
+      const info = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+      const json = await info.json();
+      setData(json);
+    } catch (error) {
+      console.log(error.message);
+      setError(error.message)
+    }
+  };
+
+  apidata();
+
+}, []);
+
     
     return(
         //return the data only if have an data instead of null
         //check the comditions before loading an data
+        
         
               <> 
             <p>{data.userId}</p>
